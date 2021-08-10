cf9cf5c1f310e880407452b2e413e7dd9be17e3d
Chris Diehl <jjchrisdiehl@gmail.com>
2021-08-10 09:55:27 -0700

    Save first iteration of scoreboard

---
+++ src/components/Player.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Counter from './Counter.js';
+
+class Player extends PureComponent{
+
+    static propTypes = {
+        changeScore: PropTypes.func,
+        removePlayer: PropTypes.func,
+        name: PropTypes.string.isRequired,
+        score: PropTypes.number.isRequired,
+        id: PropTypes.number,
+        index: PropTypes.number
+    };
+
+    render() {
+        const {
+            name,
+            id,
+            score,
+            index,
+            removePlayer,
+            changeScore
+        } = this.props;
+        return (
+            <div className="player">
+                <span className="player-name">
+                    <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+                    {name}
+                </span>
+    
+                <Counter 
+                    score={score}
+                    index={index}
+                    changeScore={changeScore}
+                />
+            </div>
+        );
+    }
+}
+
+export default Player;
